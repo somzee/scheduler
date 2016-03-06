5c17dad895d6881e68e0043b4dc8424200074e21
Sowmya <rsowmya90@gmail.com>
2016-03-06 11:19:43 -0800

    set default due date to today

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task < ActiveRecord::Base
 
-  before_create :set_default_status
+  before_create :set_defaults
 
   private
-    def set_default_status
+    def set_defaults
       self.status = "Open"
+      self.due_date ||= Date.today
     end
 
 end
