136e74a2270f64e6d8fa6f9b166f6c9d2448092e
Sowmya <rsowmya90@gmail.com>
2016-03-06 11:38:13 -0800

    Check for errors on index view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     task = Task.create(task_params)
     if task.errors.present?
-      flash[:error] = task.errors.full_messages
+      flash[:errors] = task.errors.full_messages
     end  
     redirect_to tasks_path
   end
