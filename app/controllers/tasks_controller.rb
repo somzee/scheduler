6b5b3970321a839543cea17019a87ace56c13bed
Sowmya <rsowmya90@gmail.com>
2016-03-06 23:40:22 -0800

    Made view changes to form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,6 +46,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 
   private
 
